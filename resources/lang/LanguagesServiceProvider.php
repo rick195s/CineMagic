5e91f12ec3d7c82e908f0bf8b779ba242ec4537b
Ricardo Franco <ricardosf195@gmail.com>
2022-05-03 16:04:40 +0100

    added lang resources

---
+++ resources/lang/LanguagesServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace St693ava\languages;
+
+use Illuminate\Support\ServiceProvider;
+
+class LanguagesServiceProvider extends ServiceProvider
+{
+    /**
+     * Publishes translation files.
+     *
+     * @return  void
+     */
+    public function register()
+    {
+        $this->publishes([
+            __DIR__ . '/pt_PT.json' => resource_path('lang/pt_PT.json'),
+            __DIR__ . '/pt_PT' => resource_path('lang/pt_PT'),
+        ], 'languages');
+    }
+}
---
+++ resources/lang/pt_PT/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'A senha e a confirmação devem combinar e possuir pelo menos seis caracteres.',
+    'reset' => 'Senha redefinida com sucesso!',
+    'sent' => 'Foi enviado para o seu endereço de e-mail o link de redefinição de senha!',
+    'throttled' => 'Aguarde antes de tentar novamente.',
+    'token' => 'Token de redefinição de senha inválido.',
+    'user' => "Não encontramos nenhum utilizador com esse endereço de e-mail.",
+
+];
