81f9ee803fe157ace84465f6ea1c9b8dba8f38a2
Ricardo Franco <ricardosf195@gmail.com>
2022-05-05 20:13:27 +0100

    Client extends User

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
-class Cliente extends Model
+class Cliente extends User
 {
     use HasFactory;
 }
