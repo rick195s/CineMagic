c47f04ed35c068a81a3932ecc41380d9264635c8
Ricardo Franco <ricardosf195@gmail.com>
2022-05-05 22:08:42 +0100

    HomeController index testing the Model relations

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bilhete;
+use App\Models\Cliente;
+use App\Models\Filme;
+use App\Models\Genero;
+use App\Models\Lugar;
+use App\Models\Recibo;
+use App\Models\Sala;
+use App\Models\Sessao;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +31,16 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $recibo = Bilhete::first()->recibo;
+        dump($recibo);
+
+        $bilhetes = Recibo::has('bilhetes')->first()->bilhetes->take(2);
+        foreach ($bilhetes  as $bilhete) {
+            dump($bilhete);
+        }
+
+
+
         return view('home');
     }
 }
