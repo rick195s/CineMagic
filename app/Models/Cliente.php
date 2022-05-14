d450317505250b56f9d3617fb1086fc7e4b76e7d
Ricardo Franco <ricardosf195@gmail.com>
2022-05-14 12:25:56 +0100

    added use SoftDeletes

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cliente extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     // relation clients 1:n bilhetes
     public function bilhetes()
--- app/Models/Sala.php
+++ app/Models/Sala.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Sala extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     // relation salas 1:n sessoes
     public function sessoes()
