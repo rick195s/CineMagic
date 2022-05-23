5b3d1a12fda4c3dcba6322ad51157a6608071a8c
Ricardo Franco <ricardosf195@gmail.com>
2022-05-23 20:54:25 +0100

    admin redirected to admin (solved)

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -30,7 +31,7 @@ class LoginController extends Controller
 
     public function redirectTo()
     {
-        if (auth()->user->isAdmin()) {
+        if (Auth::user()->isAdmin()) {
             return RouteServiceProvider::DASHBOARD;
         }
         return RouteServiceProvider::HOME;
