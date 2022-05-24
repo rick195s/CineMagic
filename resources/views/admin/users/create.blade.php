b4de0e2144d7ac03b21e0a943dfe29ba63db1ec9
Ricardo Franco <ricardosf195@gmail.com>
2022-05-24 19:14:32 +0100

    created create-edit user admin partials

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -13,48 +13,7 @@
                     <form action="{{ route('admin.users.store') }}" method="post">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
+                        @include('admin.users.partials.create-edit')
                         <div class="row mb-3">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
@@ -62,27 +21,6 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="tipo" class="col-md-4 col-form-label text-md-end">{{ __('User Type') }}</label>
-                            <div class="col-md-6">
-                                <input type="radio" id="A" name="tipo" value="A" {{ old('tipo') == 'A' || !old('tipo') ? 'checked' : '' }}>
-                                <label for="A">{{__("Admin")}}</label>
-
-                                <input type="radio" id="F" name="tipo" value="F" {{ old('tipo') == 'F' ? 'checked' : '' }}>
-                                <label for="F">{{__("Emplee")}}</label>
-
-                                <input type="radio" id="C" name="tipo" value="C" {{ old('tipo') == 'C' ? 'checked' : '' }}>
-                                <label for="C">{{__("Client")}}</label>
-
-                                @error('tipo')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -13,51 +13,8 @@
                     <form action="{{ route('admin.users.update', $user->id) }}" method="post">
                         @csrf
                         @method('PUT')
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                        @include('admin.users.partials.create-edit')
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') ?? $user->name }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') ?? $user->email }}" required autocomplete="email">
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-
-                        <div class="row mb-3">
-                            <label for="tipo" class="col-md-4 col-form-label text-md-end">{{ __('User Type') }}</label>
-                            <div class="col-md-6">
-                                <input type="radio" id="A" name="tipo" value="A" {{ old('tipo') == 'A' || $user->isAdmin() ? 'checked' : '' }}>
-                                <label for="A">{{__("Admin")}}</label>
-
-                                <input type="radio" id="F" name="tipo" value="F" {{ old('tipo') == 'F' || $user->isEmployee() ? 'checked' : '' }}>
-                                <label for="F">{{__("Emplee")}}</label>
-
-                                @error('tipo')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
