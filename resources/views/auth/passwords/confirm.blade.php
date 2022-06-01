e8d8ecb42a38b26640b809d9e878945aeb612d0c
Ricardo Franco <ricardosf195@gmail.com>
2022-06-01 17:26:32 +0100

    view confirm password alterada

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,49 +1,37 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+@section('title', __('Confirm Password'))
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
 
-                <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+<form method="POST" class="sign__form" action="{{ route('password.confirm') }}">
+    @csrf
 
-                    <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf
+    <a href="index.html" class="sign__logo">
+        <img src="{{asset('img/logo.svg')}}" alt="">
+    </a>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+    {{ __('Please confirm your password before continuing.') }}
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    <div class="sign__group">
+        <input id="password" placeholder="{{__('Password')}}" type="password" class="form-control sign__input @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
+        @error('password')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
     </div>
-</div>
+
+    <button type="submit" class="sign__btn">
+        {{ __('Confirm Password') }}
+    </button>
+
+    @if (Route::has('password.request'))
+    <a class="sign__text" href="{{ route('password.request') }}">
+        {{ __('Forgot Your Password?') }}
+    </a>
+    @endif
+</form>
+
 @endsection
