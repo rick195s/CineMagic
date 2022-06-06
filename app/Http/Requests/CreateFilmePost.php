9103c04dd832598dee612356bf9b072067300e25
Ricardo Franco <ricardosf195@gmail.com>
2022-06-06 16:49:28 +0100

    corrigido validacoes de criar filme

--- app/Http/Requests/CreateFilmePost.php
+++ app/Http/Requests/CreateFilmePost.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Models\Filme;
+use App\Models\Genero;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -27,7 +28,8 @@ class CreateFilmePost extends FormRequest
     {
         return [
             'titulo' => 'required|max:255',
-            'genero_code' => 'required', 'max:20',
+            // o genero_code enviado pelo form tem de estar na tabela genero
+            'genero_code' => ['required', 'max:20', Rule::in(Genero::all()->pluck('code')->toArray())],
             'ano' => 'required|numeric|min:1800|max:3000',
             'cartaz_url' => 'nullable|image|max:8192',
             'sumario' => 'required|max:500',
@@ -39,7 +41,7 @@ class CreateFilmePost extends FormRequest
     {
         return [
             'titulo' => __('Title'),
-            'genero_code' => __('Gender'),
+            'genero_code' => __('Movie Gender'),
             'ano' => __('Release Year'),
             'cartaz_url' => __('Movie Poster'),
             'sumario' => __('Summary'),
