9a547dce0679211ec701d0acc9ce4e63f6a0d95c
Ricardo Franco <ricardosf195@gmail.com>
2022-06-06 17:42:35 +0100

    alteracao da aparencia da view para escolher lugar

--- resources/views/sessao/select_seat.blade.php
+++ resources/views/sessao/select_seat.blade.php
@@ -2,70 +2,83 @@
 
 @section('title', __('Choose seat Ticket'))
 
-@section('home')
 
-<!-- home bg -->
-<div class="owl-carousel home__bg">
-    <div class="item home__cover" data-bg="{{asset('storage/cartazes/'.$sessao->filme->cartaz_url)}}"></div>
-</div>
-<!-- end home bg -->
-<div class="container ">
-    <div class="row">
-        <div class="col-12 my-4">
-            <h1 class="">{{ __('Select the seat for the session') }}</h1>
+@section('page-title')
+<section class="section section--first section--bg" data-bg="{{ asset('img/section.jpg')}}">
 
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section__wrap">
+                    <!-- section title -->
+                    <h2 class="section__title">{{__('Select the seat for the session')}}</h2>
+                    <!-- end section title -->
+
+                    <!-- breadcrumb -->
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb__item"><a href="#">Home</a></li>
+                        <li class="breadcrumb__item breadcrumb__item--active">{{__('Select the seat for the session')}}</li>
+                    </ul>
+                    <!-- end breadcrumb -->
+                </div>
+            </div>
         </div>
     </div>
-    <!-- content tabs -->
-    <div class="sign__form">
-        <div class="row ">
-            <div class="col-md-4">
-                <h2>{{$sessao->filme->titulo}}</h2>
-                <div class="card__cover w-75">
-                    <img src="{{asset('storage/cartazes/'.$sessao->filme->cartaz_url)}}" alt="">
-                </div>
-                <hr>
-                <p>{{__('Screen Room')}}: {{$sessao->sala->nome}}</p>
-                <p>{{__('Data')}}: {{$sessao->data}}</p>
-                <p>{{__('Starting Hour')}}: {{$sessao->horario_inicio}}</p>
-            </div>
-            <div class="col-md-8">
-                @foreach ($filas as $fila => $lugares)
-                <div class="row my-3">
-                    <h3>{{$fila}}</h3>
-                    <hr>
+</section>
+@endsection
 
-                    @foreach ($lugares as $lugar)
-                    <div class="col-2 mx-1">
-                        <div class="fa-4x">
-                            @if ($sessao->ocupado($lugar->id))
-                            <span class="fa-layers text-muted fa-fw">
+@section('home')
+
+<div class="container ">
+
+    <!-- content tabs -->
+    <div class="row ">
+        <div class="col-md-4">
+            <h2>{{$sessao->filme->titulo}}</h2>
+            <div class="card__cover w-75">
+                <img src="{{asset('storage/cartazes/'.$sessao->filme->cartaz_url)}}" alt="">
+            </div>
+            <hr>
+            <p>{{__('Screen Room')}}: {{$sessao->sala->nome}}</p>
+            <p>{{__('Data')}}: {{$sessao->data}}</p>
+            <p>{{__('Starting Hour')}}: {{$sessao->horario_inicio}}</p>
+        </div>
+        <div class="col-md-8">
+            @foreach ($filas as $fila => $lugares)
+            <div class="row my-3">
+                <h3>{{$fila}}</h3>
+                <hr>
+
+                @foreach ($lugares as $lugar)
+                <div class="col-2 mx-1">
+                    <div class="fa-4x">
+                        @if ($sessao->ocupado($lugar->id))
+                        <span class="fa-layers text-muted fa-fw">
+                            <i data-fa-transform="shrink-2" class="fas fa-couch"></i>
+                            <span class="fa-layers-text fa-inverse" data-fa-transform="shrink-11 up-2" style="color:black; font-weight:900">{{$lugar->posicao}}</span>
+                        </span>
+                        @else
+                        <a href="" class="seats">
+                            <span class="fa-layers  fa-fw">
                                 <i data-fa-transform="shrink-2" class="fas fa-couch"></i>
                                 <span class="fa-layers-text fa-inverse" data-fa-transform="shrink-11 up-2" style="color:black; font-weight:900">{{$lugar->posicao}}</span>
                             </span>
-                            @else
-                            <a href="" class="seats">
-                                <span class="fa-layers  fa-fw">
-                                    <i data-fa-transform="shrink-2" class="fas fa-couch"></i>
-                                    <span class="fa-layers-text fa-inverse" data-fa-transform="shrink-11 up-2" style="color:black; font-weight:900">{{$lugar->posicao}}</span>
-                                </span>
-                            </a>
-                            @endif
-
-                        </div>
+                        </a>
+                        @endif
 
                     </div>
 
-                    @endforeach
-
                 </div>
-                @endforeach
-            </div>
 
+                @endforeach
+
+            </div>
+            @endforeach
         </div>
+
     </div>
-    <!-- end content tabs -->
 </div>
+<!-- end content tabs -->
 
 <!-- end content -->
 
