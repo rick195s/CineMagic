157676c2545531419a65fe246f7da7c63cc8bfec
Ricardo Franco <ricardosf195@gmail.com>
2022-06-18 15:34:49 +0100

    atualizacao das estatisticas

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -102,16 +102,31 @@
             </div>
 
             <div class="col-xl-6 col-xxl-7">
-                <div class="card flex-fill w-100">
+                <div class="card flex-fill">
                     <div class="card-header">
 
-                        <h5 class="card-title mb-0">Recent Movement</h5>
+                        <h5 class="card-title mb-0">{{ __('Latest Sales') }}</h5>
                     </div>
-                    <div class="card-body py-3">
-                        <div class="chart chart-sm">
-                            <canvas id="chartjs-dashboard-line"></canvas>
-                        </div>
-                    </div>
+                    <table class="table table-hover my-0">
+                        <thead>
+                            <tr>
+                                <th>{{ __('Name') }}</th>
+                                <th class="d-none d-xl-table-cell">{{ __('Date') }}</th>
+                                <th class="d-none d-xl-table-cell">{{ __('Value') }}</th>
+                                <th>{{ __('Payment Type') }}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($estatisticas->getLastSales() as $recibo)
+                                <tr>
+                                    <td>{{ $recibo->nome_cliente }}</td>
+                                    <td class="d-none d-xl-table-cell">{{ $recibo->data }}</td>
+                                    <td class="d-none d-xl-table-cell">{{ $recibo->preco_total_com_iva }}€</td>
+                                    <td><span class="badge bg-success">{{ $recibo->tipo_pagamento }}</span></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -174,113 +189,9 @@
         </div>
 
         <div class="row">
-            <div class="col-12 col-lg-8 col-xxl-9 d-flex">
-                <div class="card flex-fill">
-                    <div class="card-header">
 
-                        <h5 class="card-title mb-0">{{ __('Latest Sales') }}</h5>
-                    </div>
-                    <table class="table table-hover my-0">
-                        <thead>
-                            <tr>
-                                <th>{{ __('Name') }}</th>
-                                <th class="d-none d-xl-table-cell">{{ __('Date') }}</th>
-                                <th class="d-none d-xl-table-cell">{{ __('Value') }}</th>
-                                <th>{{ __('Payment Type') }}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($estatisticas->getLastSales() as $recibo)
-                                <tr>
-                                    <td>{{ $recibo->nome_cliente }}</td>
-                                    <td class="d-none d-xl-table-cell">{{ $recibo->data }}</td>
-                                    <td class="d-none d-xl-table-cell">{{ $recibo->preco_total_com_iva }}€</td>
-                                    <td><span class="badge bg-success">{{ $recibo->tipo_pagamento }}</span></td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <div class="col-12 col-lg-4 col-xxl-3 d-flex">
-                <div class="card flex-fill w-100">
-                    <div class="card-header">
 
-                        <h5 class="card-title mb-0">Monthly Sales</h5>
-                    </div>
-                    <div class="card-body d-flex w-100">
-                        <div class="align-self-center chart chart-lg">
-                            <canvas id="chartjs-dashboard-bar"></canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
-
-
-        <div class="row">
-            <div class="col-12 col-md-6 col-xxl-3 d-flex order-2 order-xxl-3">
-                <div class="card flex-fill w-100">
-                    <div class="card-header">
-
-                        <h5 class="card-title mb-0">Browser Usage</h5>
-                    </div>
-                    <div class="card-body d-flex">
-                        <div class="align-self-center w-100">
-                            <div class="py-3">
-                                <div class="chart chart-xs">
-                                    <canvas id="chartjs-dashboard-pie"></canvas>
-                                </div>
-                            </div>
-
-                            <table class="table mb-0">
-                                <tbody>
-                                    <tr>
-                                        <td>Chrome</td>
-                                        <td class="text-end">4306</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Firefox</td>
-                                        <td class="text-end">3801</td>
-                                    </tr>
-                                    <tr>
-                                        <td>IE</td>
-                                        <td class="text-end">1689</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-12 col-xxl-6 d-flex order-3 order-xxl-2">
-                <div class="card flex-fill w-100">
-                    <div class="card-header">
-
-                        <h5 class="card-title mb-0">Real-Time</h5>
-                    </div>
-                    <div class="card-body px-4">
-                        <div id="world_map" style="height:350px;"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-xxl-3 d-flex order-1 order-xxl-1">
-                <div class="card flex-fill">
-                    <div class="card-header">
-
-                        <h5 class="card-title mb-0">Calendar</h5>
-                    </div>
-                    <div class="card-body d-flex">
-                        <div class="align-self-center w-100">
-                            <div class="chart">
-                                <div id="datetimepicker-dashboard"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
 
     </div>
 @endsection
