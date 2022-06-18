81119719216183aadfd2c976c2188279aa5b8d5f
Ricardo <ricardo.casal@hotmail.com>
2022-06-18 02:18:04 +0100

    Update Client Implementado, Profile

---
+++ app/Http/Requests/UpdateClientPost.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Cliente;
+use App\Models\User;
+use Illuminate\Validation\Rule;
+
+class UpdateClientPost extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('update',Cliente::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['string', 'max:255'],
+            'nif' => ['nullable','max:9'],
+            'tipo_pagamento' => ['nullable', Rule::in(['MBWAY', 'VISA','NULL','PAYPAL'])],
+            'foto_url' => ['nullable', 'image', 'max:8192']
+        ];
+    }
+}
